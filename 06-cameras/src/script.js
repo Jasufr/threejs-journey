66f842b0f96e98bcd3abecb81a56cb847a2fb4c7
justin etienne <juetienne7@gmail.com>
2024-07-19 18:45:43 +0900

    lesson 6 done

--- 06-cameras/src/script.js
+++ 06-cameras/src/script.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 /**
  * Cursor
@@ -47,6 +49,12 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -64,9 +72,13 @@ const tick = () =>
     // mesh.rotation.y = elapsedTime;
 
     //Update camera
-    camera.position.x = cursor.x * 5
-    camera.position.y = cursor.y * 5
-    camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.lookAt(mesh.position)
+
+    //Update controls
+    controls.update()
 
     // Render
     renderer.render(scene, camera)

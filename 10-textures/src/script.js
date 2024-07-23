ce6947f758ce0ea5af52547172d15a7a5719ee17
justin etienne <juetienne7@gmail.com>
2024-07-23 20:56:33 +0900

    lesson 10 done

--- 10-textures/src/script.js
+++ 10-textures/src/script.js
@@ -33,7 +33,10 @@ loadingManager.onError = () => {
 }
 
 const textureLoader = new THREE.TextureLoader(loadingManager)
-const colorTexture = textureLoader.load('/textures/door/color.jpg')
+// const colorTexture = textureLoader.load('/textures/door/color.jpg')
+// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
+// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+const colorTexture = textureLoader.load('/textures/minecraft.png')
 colorTexture.colorSpace = THREE.SRGBColorSpace
 
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
@@ -42,6 +45,22 @@ const normalTexture = textureLoader.load('/textures/door/normal.jpg')
 const ambiantOcclusionTexture = textureLoader.load('/textures/door/ambiantOcclusion.jpg')
 const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const roughtnessTexture = textureLoader.load('/textures/door/roughtness.jpg')
+
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+colorTexture.generateMipmaps = false
+colorTexture.minFilter = THREE.NearestFilter
+colorTexture.magFilter = THREE.NearestFilter
 
 /**
  * Base

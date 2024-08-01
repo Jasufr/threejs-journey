75bc6479285353ea223dfc7aa47055335a40b354
justin etienne <juetienne7@gmail.com>
2024-08-01 15:00:59 +0900

    lesson 19 done

--- chapter_02_classic_techniques/19-scroll-based-animation/src/script.js
+++ chapter_02_classic_techniques/19-scroll-based-animation/src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import GUI from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Debug
@@ -14,6 +15,7 @@ gui
     .addColor(parameters, 'materialColor')
     .onChange(() => {
       material.color.set(parameters.materialColor)
+      particlesMaterial.color.set(parameters.materialColor)
     })
 
 /**
@@ -67,6 +69,33 @@ mesh3.position.x = 2
 scene.add(mesh1, mesh2, mesh3)
 
 const sectionMeshes = [ mesh1, mesh2, mesh3 ]
+
+/**
+ * Particles
+ */
+//Geometry
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+
+for(let i = 0; i < particlesCount; i ++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+  positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+//Material
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03
+})
+
+//Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Lights
@@ -124,9 +153,26 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Scroll
  */
 let scrollY = window.scrollY
+let currentSection = 0
 
 window.addEventListener('scroll', () => {
   scrollY = window.scrollY
+
+  const newSection = Math.round(scrollY / sizes.height)
+
+  if(newSection !== currentSection) {
+    currentSection = newSection
+
+    gsap.to(
+      sectionMeshes[currentSection].rotation, {
+        duration: 1.5,
+        ease: 'power2.inOut',
+        x: '+=6',
+        y: '+=3',
+        z: '+=1.5'
+      }
+    )
+  }
 })
 
 /**
@@ -145,23 +191,27 @@ window.addEventListener('mousemove', (event) => {
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
 
     //Animate Camera
     camera.position.y = - scrollY / sizes.height * objectsDistance
 
-    const parallaxX = cursor.x
-    const parallaxY = - cursor.y
-    cameraGroup.position.x = parallaxX
-    cameraGroup.position.y = parallaxY
+    const parallaxX = cursor.x * 0.5
+    const parallaxY = - cursor.y * 0.5
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
     //Animate Meshes
     for(const mesh of sectionMeshes) {
-      mesh.rotation.x = elapsedTime * 0.1
-      mesh.rotation.y = elapsedTime * 0.12
+      mesh.rotation.x += deltaTime * 0.1
+      mesh.rotation.y += deltaTime * 0.12
     }
 
     // Render

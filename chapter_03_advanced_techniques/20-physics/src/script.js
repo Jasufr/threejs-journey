1432cefb5136ba67dd4318aebc227e411af90281
justin etienne <juetienne7@gmail.com>
2024-08-06 16:55:52 +0900

    lesson 20 remove things done

--- chapter_03_advanced_techniques/20-physics/src/script.js
+++ chapter_03_advanced_techniques/20-physics/src/script.js
@@ -19,6 +19,21 @@ debugObject.createBox = () => {
 }
 gui.add(debugObject, 'createBox')
 
+debugObject.reset = () => {
+  for(const object of objectsToUpdate) {
+    //Remove body
+    object.body.removeEventListener('collide', playHitSound)
+    world.removeBody(object.body)
+
+    //Remove mesh
+    scene.remove(object.mesh)
+
+  }
+  objectsToUpdate.splice(0, objectsToUpdate.length)
+}
+
+gui.add(debugObject, 'reset')
+
 /**
  * Base
  */
@@ -27,6 +42,21 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Sounds
+ */
+const hitSound = new Audio('/sounds/hit.mp3')
+
+const playHitSound = (collision) => {
+  const impactStrength = collision.contact.getImpactVelocityAlongNormal()
+
+  if(impactStrength > 1.5) {
+    hitSound.volume = Math.random()
+    hitSound.currentTime = 0
+    hitSound.play()
+  }
+}
 
 /**
  * Textures
@@ -48,6 +78,8 @@ const environmentMapTexture = cubeTextureLoader.load([
  */
 //World
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
 world.gravity.set(0, -9.82, 0)
 
 //Materials
@@ -216,6 +248,7 @@ const createSphere = (radius, position) => {
     material: defaultMaterial
   })
   body.position.copy(position)
+  body.addEventListener('collide', playHitSound)
   world.addBody(body)
 
   // Save in objectsToUpdate
@@ -256,6 +289,7 @@ const createBox = (width, height, depth, position) => {
     material: defaultMaterial
   })
   body.position.copy(position)
+  body.addEventListener('collide', playHitSound)
   world.addBody(body)
 
   // Save in objectsToUpdate

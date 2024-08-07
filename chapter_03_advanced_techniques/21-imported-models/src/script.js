1cc620b45c3cc417474bd15d9bc80f031b47c834
justin etienne <juetienne7@gmail.com>
2024-08-07 21:00:40 +0900

    lesson 21 done

--- chapter_03_advanced_techniques/21-imported-models/src/script.js
+++ chapter_03_advanced_techniques/21-imported-models/src/script.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /**
  * Base
@@ -18,10 +19,16 @@ const scene = new THREE.Scene()
 /**
  * Models
  */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
 const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+let mixer = null
 
 gltfLoader.load(
-  '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+  '/models/Fox/glTF/Fox.gltf',
   (gltf) => {
     // console.log('success');
 
@@ -34,7 +41,11 @@ gltfLoader.load(
     // for(const child of children) {
     //   scene.add(child)
     // }
+    mixer = new THREE.AnimationMixer(gltf.scene)
+    const action = mixer.clipAction(gltf.animations[2])
+    action.play()
 
+    gltf.scene.scale.set(0.025, 0.025, 0.025)
     scene.add(gltf.scene)
   },
   // () => {
@@ -135,6 +146,11 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - previousTime
     previousTime = elapsedTime
+
+    // Update Mixer
+    if(mixer !== null) {
+      mixer.update(deltaTime)
+    }
 
     // Update controls
     controls.update()

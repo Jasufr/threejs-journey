0a82948d16aca50e844b7ce61fe2664daf798489
justin etienne <juetienne7@gmail.com>
2024-08-07 17:00:08 +0900

    lesson 21 add loaded model to scene done

--- chapter_03_advanced_techniques/21-imported-models/src/script.js
+++ chapter_03_advanced_techniques/21-imported-models/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -13,6 +14,36 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+gltfLoader.load(
+  '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+  (gltf) => {
+    // console.log('success');
+
+    // while(gltf.scene.children.length) {
+    //   scene.add(gltf.scene.children[0])
+    // }
+
+    // const children =  [...gltf.scene.children]
+
+    // for(const child of children) {
+    //   scene.add(child)
+    // }
+
+    scene.add(gltf.scene)
+  },
+  // () => {
+  //   console.log('progress');
+  // },
+  // () => {
+  //   console.log('error');
+  // }
+)
 
 /**
  * Floor

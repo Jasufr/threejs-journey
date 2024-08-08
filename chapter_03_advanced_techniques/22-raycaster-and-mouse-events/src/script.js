b6b44075529cd21bc78750feb5ba6c1779b7a51e
justin etienne <juetienne7@gmail.com>
2024-08-08 14:47:40 +0900

    lesson 22 done

--- chapter_03_advanced_techniques/22-raycaster-and-mouse-events/src/script.js
+++ chapter_03_advanced_techniques/22-raycaster-and-mouse-events/src/script.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -128,6 +129,30 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Model
+ */
+const gltfLoader = new GLTFLoader()
+
+let model = null
+gltfLoader.load('./models/Duck/glTF-Binary/Duck.glb', (gltf) => {
+  model = gltf.scene
+  model.position.y = - 1.2
+  scene.add(model)
+})
+
+/**
+ * Lights
+ */
+//Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.9)
+scene.add(ambientLight)
+
+//Directional light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 2.1)
+directionalLight.position.set(1, 2, 3)
+scene.add(directionalLight)
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -168,6 +193,18 @@ const tick = () => {
       console.log('mouse leave');
     }
     currentIntersect = null
+  }
+
+  //Test intersect with model
+  if(model) {
+    const modelIntersects = raycaster.intersectObject(model)
+
+    if(modelIntersects.length) {
+      model.scale.set(1.2, 1.2, 1.2)
+    } else {
+      model.scale.set(1, 1, 1)
+
+    }
   }
 
     //Ray in center of screen, turns sphere blue when the intersect with it

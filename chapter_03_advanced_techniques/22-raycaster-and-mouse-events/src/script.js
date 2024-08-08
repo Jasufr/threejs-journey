7dee917c1d8e9476aecd8373535d4b884ae05b92
justin etienne <juetienne7@gmail.com>
2024-08-08 13:58:50 +0900

    lesson 22 raycaster mouse event done

--- chapter_03_advanced_techniques/22-raycaster-and-mouse-events/src/script.js
+++ chapter_03_advanced_techniques/22-raycaster-and-mouse-events/src/script.js
@@ -18,19 +18,19 @@ const scene = new THREE.Scene()
  * Objects
  */
 const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object1.position.x = - 2
 
 const object2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 
 const object3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
 object3.position.x = 2
 
@@ -63,23 +63,47 @@ const raycaster = new THREE.Raycaster()
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+window.addEventListener('mousemove', (event) => {
+  mouse.x = event.clientX / sizes.width * 2 - 1
+  mouse.y = - (event.clientY / sizes.height * 2 - 1)
+})
+
+window.addEventListener('click', () => {
+  if(currentIntersect) {
+    switch(currentIntersect.object) {
+      case object1:
+        console.log('click object1');
+        break
+      case object2:
+        console.log('click object2');
+        break
+      case object3:
+        console.log('click object3');
+        break
+    }
+  }
 })
 
 /**
@@ -98,7 +122,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -108,41 +132,70 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+let currentIntersect = null
 
-    // Animate objects
-    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
-    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
-    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // // Cast a ray
-    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    // const rayDirection = new THREE.Vector3(1, 0, 0)
-    // rayDirection.normalize()
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
-    // raycaster.set(rayOrigin, rayDirection)
+  // // Cast a ray
 
-    // const objectsToTest = [object1, object2, object3]
-    // const intersects = raycaster.intersectObjects(objectsToTest)
+    //Turns sphere blue when mouse intersect
+  raycaster.setFromCamera(mouse, camera)
+  const objectsToTest = [object1, object2, object3]
+  const intersects = raycaster.intersectObjects(objectsToTest)
 
-    // for(const object of objectsToTest) {
-    //   object.material.color.set('#ff0000')
-    // }
+  for (const object of objectsToTest) {
+    object.material.color.set('#ff0000')
+  }
 
-    // for(const intersect of intersects) {
-    //   intersect.object.material.color.set('#0000ff')
-    // }
+  for (const intersect of intersects) {
+    intersect.object.material.color.set('#0000ff')
+  }
 
-    // Update controls
-    controls.update()
+  if(intersects.length) {
+    if(currentIntersect === null) {
+      console.log('mouse enter');
+    }
 
-    // Render
-    renderer.render(scene, camera)
+    currentIntersect = intersects[0]
+  } else {
+    if(currentIntersect) {
+      console.log('mouse leave');
+    }
+    currentIntersect = null
+  }
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    //Ray in center of screen, turns sphere blue when the intersect with it
+  // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+  // const rayDirection = new THREE.Vector3(1, 0, 0)
+  // rayDirection.normalize()
+
+  // raycaster.set(rayOrigin, rayDirection)
+
+  // const objectsToTest = [object1, object2, object3]
+  // const intersects = raycaster.intersectObjects(objectsToTest)
+
+  // for(const object of objectsToTest) {
+  //   object.material.color.set('#ff0000')
+  // }
+
+  // for(const intersect of intersects) {
+  //   intersect.object.material.color.set('#0000ff')
+  // }
+
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()

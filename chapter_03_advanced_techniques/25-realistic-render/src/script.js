c6960e3fd8d6377678c00871c9bacc83e6748cbd
justin etienne <juetienne7@gmail.com>
2024-08-14 11:58:13 +0900

    lesson 25 shadows done

--- chapter_03_advanced_techniques/25-realistic-render/src/script.js
+++ chapter_03_advanced_techniques/25-realistic-render/src/script.js
@@ -32,6 +32,8 @@ const updateAllMaterials = () =>
         if(child.isMesh)
         {
             // Activate shadow here
+            child.castShadow = true
+            child.receiveShadow = true
         }
     })
 }
@@ -55,6 +57,32 @@ rgbeLoader.load('/environmentMaps/0/2k.hdr', (environmentMap) =>
     scene.background = environmentMap
     scene.environment = environmentMap
 })
+
+/**
+ * Directional Light
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 6)
+directionalLight.position.set(- 4, 6.5, 2.5)
+scene.add(directionalLight)
+
+gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
+gui.add(directionalLight.position, 'x').min(-10).max(10).step(0.001).name('lightX')
+gui.add(directionalLight.position, 'y').min(-10).max(10).step(0.001).name('lightY')
+gui.add(directionalLight.position, 'z').min(-10).max(10).step(0.001).name('lightZ')
+
+// Shadows
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(512, 512)
+gui.add(directionalLight, 'castShadow')
+
+// // Helper
+// const directionalLightHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(directionalLightHelper)
+
+// Target
+directionalLight.target.position.set(0, 4, 0)
+directionalLight.target.updateWorldMatrix()
 
 /**
  * Models
@@ -111,10 +139,28 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Tone mapping
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 3
+
+gui.add(renderer, 'toneMapping', {
+  No: THREE.NoToneMapping,
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping
+})
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
+
+// Shadows
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
 
 /**
  * Animate

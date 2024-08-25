2466ee78618945b62a02ced548ce370fa4951649
justin etienne <juetienne7@gmail.com>
2024-08-25 16:10:43 +0900

    lesson 28 pattern 7 done

--- chapter_04_shaders/28-shader-patterns/src/shaders/test/fragment.glsl
+++ chapter_04_shaders/28-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,4 +1,24 @@
+varying vec2 vUv;
+
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    // Pattern 1
+    // gl_FragColor = vec4(vUv, 1.0, 1.0);
+
+    // Pattern 2
+    // gl_FragColor = vec4(vUv, 0.0, 1.0);
+
+    // Pattern 3
+    // float strength = vUv.x;
+
+    // Pattern 4
+    // float strength = vUv.y;
+
+    // Pattern 5
+    // float strength = 1.0 - vUv.y;
+
+    // Pattern 6
+    float strength = vUv.y * 10.0;
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }

75a6cb817f64cb7b553c34eb1fee5f58ede6cbef
justin etienne <juetienne7@gmail.com>
2024-08-27 19:33:33 +0900

    lesson 28 done

--- chapter_04_shaders/28-shader-patterns/src/shaders/test/fragment.glsl
+++ chapter_04_shaders/28-shader-patterns/src/shaders/test/fragment.glsl
@@ -285,9 +285,12 @@ void main()
     // Pattern 50
     float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
 
+    // Clamp the strength
+    strength = clamp(strength, 0.0, 1.0);
+
     // Colored version
     vec3 blackColor = vec3(0.0);
-    vec3 uvColor = vec3(vUv, 1.0);
+    vec3 uvColor = vec3(vUv, 0.5);
     vec3 mixedColor = mix(blackColor, uvColor, strength);
     gl_FragColor = vec4(mixedColor, 1.0);
 

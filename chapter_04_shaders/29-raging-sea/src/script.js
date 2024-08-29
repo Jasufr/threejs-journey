2400d75ffb996359e55923a0a78c0dc8d1f6baec
justin etienne <juetienne7@gmail.com>
2024-08-29 12:06:48 +0900

    lesson 29 done

--- chapter_04_shaders/29-raging-sea/src/script.js
+++ chapter_04_shaders/29-raging-sea/src/script.js
@@ -21,7 +21,7 @@ const scene = new THREE.Scene()
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
 
 // Color
 // debugObject.depthColor = '#186691'
@@ -39,6 +39,11 @@ const waterMaterial = new THREE.ShaderMaterial({
     uBigWavesElevation: { value: 0.2 },
     uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
     uBigWavesSpeed : { value : 0.75 },
+
+    uSmallWavesElevation: { value: 0.15},
+    uSmallWavesFrequency: { value: 3},
+    uSmallWavesSpeed: { value: 0.2},
+    uSmallWavesIterations: { value: 4},
 
     uDepthColor: { value: new THREE.Color(debugObject.depthColor) },
     uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
@@ -62,6 +67,12 @@ gui.addColor(debugObject, 'surfaceColor').name('surfaceColor').onChange(() => {
 })
 gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
 gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
+
+gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
+gui.add(waterMaterial.uniforms.uSmallWavesFrequency, 'value').min(0).max(30).step(0.001).name('uSmallWavesFrequency')
+gui.add(waterMaterial.uniforms.uSmallWavesSpeed, 'value').min(0).max(4).step(0.001).name('uSmallWavesSpeed')
+gui.add(waterMaterial.uniforms.uSmallWavesIterations, 'value').min(0).max(8).step(1).name('uSmallWavesIterations')
+
 
 
 

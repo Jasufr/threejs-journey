8245936ab07ab5d92f4a52ca591e65d5aafac8df
justin etienne <juetienne7@gmail.com>
2024-09-03 15:02:43 +0900

    lesson 34 proportionate part done

--- chapter_04_shaders/34-fireworks-shaders/src/script.js
+++ chapter_04_shaders/34-fireworks-shaders/src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import fireworkVertexShader from './shader/firework/vertex.glsl'
+import fireworkFragmentShader from './shader/firework/fragment.glsl'
 
 /**
  * Base
@@ -22,14 +24,18 @@ const textureLoader = new THREE.TextureLoader()
  */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2)
 }
+sizes.resolution = new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
 window.addEventListener('resize', () =>
 {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+    sizes.resolution.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -37,7 +43,7 @@ window.addEventListener('resize', () =>
 
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setPixelRatio(sizes.pixelRatio)
 })
 
 /**
@@ -60,16 +66,47 @@ const renderer = new THREE.WebGLRenderer({
     antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(sizes.pixelRatio)
 
 /**
- * Test
+ * Fireworks
  */
-const test = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+const createFireworks = (count, position, size) => {
+  // Geometry
+  const positionArray = new Float32Array(count * 3)
+
+  for(let i = 0; i < count; i++) {
+    const i3 = i * 3
+
+    positionArray[i3 + 0] = Math.random() - 0.5
+    positionArray[i3 + 1] = Math.random() - 0.5
+    positionArray[i3 + 2] = Math.random() - 0.5
+  }
+
+  const geometry = new THREE.BufferGeometry()
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute(positionArray, 3))
+
+  // Material
+  const material = new THREE.ShaderMaterial({
+    vertexShader: fireworkVertexShader,
+    fragmentShader: fireworkFragmentShader,
+    uniforms: {
+      uSize: new THREE.Uniform(size),
+      uResolution: new THREE.Uniform(sizes.resolution)
+    }
+  })
+
+  // Points
+  const firework = new THREE.Points(geometry, material)
+  firework.position.copy(position)
+  scene.add(firework)
+}
+
+createFireworks(
+  100,                          // Count
+  new THREE.Vector3(),          // Position
+  0.5                            // Size
 )
-scene.add(test)
 
 /**
  * Animate

64b266c89c9917c193f66e21965d36fad24c8a2c
justin etienne <juetienne7@gmail.com>
2024-09-04 20:39:34 +0900

    lesson 35 done

--- chapter_04_shaders/35-lights-shading-shaders/src/script.js
+++ chapter_04_shaders/35-lights-shading-shaders/src/script.js
@@ -131,6 +131,7 @@ gltfLoader.load(
 /**
  * Lights helpers
  */
+// Directional light helper
 const directionalLightHelper = new THREE.Mesh(
   new THREE.PlaneGeometry(),
   new THREE.MeshBasicMaterial()
@@ -139,6 +140,24 @@ directionalLightHelper.material.color.setRGB(0.1, 0.1, 1)
 directionalLightHelper.material.side = THREE.DoubleSide
 directionalLightHelper.position.set(0, 0, 3)
 scene.add(directionalLightHelper)
+
+// Point light helper
+const pointLightHelper = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+pointLightHelper.material.color.setRGB(1, 0.1, 0.1)
+pointLightHelper.position.set(0, 2.5, 0)
+scene.add(pointLightHelper)
+
+// Point light helper
+const pointLightHelper2 = new THREE.Mesh(
+  new THREE.IcosahedronGeometry(0.1, 2),
+  new THREE.MeshBasicMaterial()
+)
+pointLightHelper2.material.color.setRGB(0.1, 1.0, 0.5)
+pointLightHelper2.position.set(2, 2, 2)
+scene.add(pointLightHelper2)
 
 /**
  * Animate
--- chapter_04_shaders/35-lights-shading-shaders/src/shaders/shading/fragment.glsl
+++ chapter_04_shaders/35-lights-shading-shaders/src/shaders/shading/fragment.glsl
@@ -5,6 +5,8 @@ varying vec3 vPosition;
 
 #include ../includes/ambiantLight.glsl
 #include ../includes/directionalLight.glsl
+#include ../includes/pointLight.glsl
+
 
 void main()
 {
@@ -25,6 +27,26 @@ void main()
       vec3(0.0, 0.0, 3.0),   // Light position
       viewDirection,         // View direction
       20.0                   // Specular power
+    );
+    light += pointLight(
+      vec3(1.0, 0.1, 0.1),   // Light color
+      1.0,                   // Light intensity
+      normal,                // Normal
+      vec3(0.0, 2.5, 0.0),   // Light position
+      viewDirection,         // View direction
+      20.0,                  // Specular power
+      vPosition,             // Position
+      0.25                   // Light decay
+    );
+    light += pointLight(
+      vec3(0.0, 1.0, 0.5),   // Light color
+      1.0,                   // Light intensity
+      normal,                // Normal
+      vec3(2.0, 2.0, 2.0),   // Light position
+      viewDirection,         // View direction
+      20.0,                  // Specular power
+      vPosition,             // Position
+      0.2                    // Light decay
     );
     color *= light;
 

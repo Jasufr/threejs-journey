d283b81e60938afbbd0350d591d0d1a1e36658c7
justin etienne <juetienne7@gmail.com>
2024-09-09 19:20:24 +0900

    lesson 37 done

--- chapter_04_shaders/37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
+++ chapter_04_shaders/37-halftone-shading-shaders/src/shaders/halftone/fragment.glsl
@@ -1,11 +1,37 @@
 uniform vec3 uColor;
 uniform vec2 uResolution;
+uniform float uShadowRepetitions;
+uniform vec3 uShadowColor;
+uniform float uLightRepetitions;
+uniform vec3 uLightColor;
 
 varying vec3 vNormal;
 varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
+
+vec3 halftone(
+  vec3 color,
+  float repetitions,
+  vec3 direction,
+  float low,
+  float high,
+  vec3 pointColor,
+  vec3 normal
+) {
+    float intensity = dot(normal, direction);
+    intensity = smoothstep(low, high, intensity);
+
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv *= repetitions;
+    uv = mod(uv, 1.0);
+
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point);
+
+    return mix(color, pointColor, point);
+}
 
 void main()
 {
@@ -33,17 +59,28 @@ void main()
     color *= light;
 
     // Halftone
-    float repetitions = 50.0;
+    color = halftone(
+      color,
+      uShadowRepetitions,
+      vec3(0.0, -1.0, 0.0),
+      - 0.8,
+      1.5,
+      uShadowColor,
+      normal
+    );
 
-    vec2 uv = gl_FragCoord.xy / uResolution.y;
-    uv *= repetitions;
-    uv = mod(uv, 1.0);
-
-    float point = distance(uv, vec2(0.5));
-    point = 1.0 - step(0.5, point);
+    color = halftone(
+      color,
+      uLightRepetitions,
+      vec3(1.0, 1.0, 0.0),
+      0.5,
+      1.5,
+      uLightColor,
+      normal
+    );
 
     // Final color
-    gl_FragColor = vec4(point, point, point, 1.0);
+    gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }

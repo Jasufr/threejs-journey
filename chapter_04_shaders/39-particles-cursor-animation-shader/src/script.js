c5f8ce470b75e2ad2fda973efca291ca082a22dd
justin etienne <juetienne7@gmail.com>
2024-09-12 16:55:04 +0900

    lesson 39 done

--- chapter_04_shaders/39-particles-cursor-animation-shader/src/script.js
+++ chapter_04_shaders/39-particles-cursor-animation-shader/src/script.js
@@ -84,20 +84,16 @@ document.body.append(displacement.canvas)
 
 // Context
 displacement.context = displacement.canvas.getContext('2d')
-// displacement.context.fillStyle = 'red'
 displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
 
 // Glow image
 displacement.glowImage = new Image()
 displacement.glowImage.src = './glow.png'
-// window.setTimeout(() => {
-//   displacement.context.drawImage(displacement.glowImage, 20, 20, 32, 32)
-// }, 1000);
 
 // Interactive plane
 displacement.interactivePlane = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshBasicMaterial({ color: 'red' })
+  new THREE.MeshBasicMaterial({ color: 'red', side: THREE.DoubleSide })
 )
 displacement.interactivePlane.visible = false
 scene.add(displacement.interactivePlane)
@@ -108,6 +104,7 @@ displacement.raycaster = new THREE.Raycaster()
 // Coordinates
 displacement.screenCursor = new THREE.Vector2(9999, 9999)
 displacement.canvasCursor = new THREE.Vector2(9999, 9999)
+displacement.canvasCursorPrevious = new THREE.Vector2(9999, 9999)
 
 
 window.addEventListener('pointermove', (event) => {
@@ -123,6 +120,8 @@ displacement.texture = new THREE.CanvasTexture(displacement.canvas)
  * Particles
  */
 const particlesGeometry = new THREE.PlaneGeometry(10, 10, 128, 128)
+particlesGeometry.setIndex(null)
+particlesGeometry.deleteAttribute('normal')
 
 const intensitiesArray = new Float32Array(particlesGeometry.attributes.position.count)
 const anglesArray = new Float32Array(particlesGeometry.attributes.position.count)
@@ -176,10 +175,15 @@ const tick = () => {
   displacement.context.globalAlpha = 0.02
   displacement.context.fillRect(0, 0, displacement.canvas.width, displacement.canvas.height)
 
+  // Speed alpha
+  const cursorDistance = displacement.canvasCursorPrevious.distanceTo(displacement.canvasCursor)
+  displacement.canvasCursorPrevious.copy(displacement.canvasCursor)
+  const alpha = Math.min(cursorDistance * 0.1, 1)
+
   // Draw glow
   const glowSize = displacement.canvas.width * 0.25
   displacement.context.globalCompositeOperation = 'lighten'
-  displacement.context.globalAlpha = 1
+  displacement.context.globalAlpha = alpha
   displacement.context.drawImage(
     displacement.glowImage,
     displacement.canvasCursor.x - glowSize * 0.5,

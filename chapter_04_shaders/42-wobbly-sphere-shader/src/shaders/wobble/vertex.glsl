f5909888c8a75e35605b94e9359282ca7062bbe9
justin etienne <juetienne7@gmail.com>
2024-09-17 15:48:50 +0900

    lesson 42 done

--- chapter_04_shaders/42-wobbly-sphere-shader/src/shaders/wobble/vertex.glsl
+++ chapter_04_shaders/42-wobbly-sphere-shader/src/shaders/wobble/vertex.glsl
@@ -2,14 +2,25 @@ uniform float uTime;
 uniform float uPositionFrequency;
 uniform float uTimeFrequency;
 uniform float uStrength;
+uniform float uWarpPositionFrequency;
+uniform float uWarpTimeFrequency;
+uniform float uWarpStrength;
 
 attribute vec4 tangent;
+
+varying float vWobble;
 
 #include ../includes/simplexNoise4d.glsl
 
 float getWobble(vec3 position) {
+    vec3 warpedPosition = position;
+    warpedPosition += simplexNoise4d(vec4(
+      position * uWarpPositionFrequency,
+      uTime * uWarpTimeFrequency
+    )) * uWarpStrength;
+
     return simplexNoise4d(vec4(
-    position * uPositionFrequency, // XYZ
+    warpedPosition * uPositionFrequency, // XYZ
     uTime * uTimeFrequency // W
   )) * uStrength;
 }
@@ -32,4 +43,7 @@ void main() {
   vec3 toA = normalize(positionA - csm_Position);
   vec3 toB = normalize(positionB - csm_Position);
   csm_Normal = cross(toA, toB);
+
+  // Varying
+  vWobble = wobble / uStrength;
 }

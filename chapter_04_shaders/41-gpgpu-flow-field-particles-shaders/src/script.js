6dbb98985e1a5c78e4d3df6b94534ce2d0fe1dd6
justin etienne <juetienne7@gmail.com>
2024-09-17 09:06:42 +0900

    lesson 41 flow field animation part done

--- chapter_04_shaders/41-gpgpu-flow-field-particles-shaders/src/script.js
+++ chapter_04_shaders/41-gpgpu-flow-field-particles-shaders/src/script.js
@@ -119,6 +119,9 @@ for(let i = 0; i < baseGeometry.count; i++) {
 gpgpu.particlesVariable =  gpgpu.computation.addVariable('uParticles', gpgpuParticlesShader, baseParticlesTexture)
 gpgpu.computation.setVariableDependencies(gpgpu.particlesVariable, [ gpgpu.particlesVariable ])
 
+// Uniforms
+gpgpu.particlesVariable.material.uniforms.uTime = new THREE.Uniform(0)
+
 // Init
 gpgpu.computation.init()
 
@@ -203,6 +206,7 @@ const tick = () =>
     controls.update()
 
     // Gpgpu update
+    gpgpu.particlesVariable.material.uniforms.uTime.value = elapsedTime
     gpgpu.computation.compute()
     particles.material.uniforms.uParticlesTexture.value = gpgpu.computation.getCurrentRenderTarget(gpgpu.particlesVariable).texture
 

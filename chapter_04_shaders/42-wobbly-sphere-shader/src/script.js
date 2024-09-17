ee7770782e8766601d8da3647ce11da97eaa4439
justin etienne <juetienne7@gmail.com>
2024-09-17 15:04:23 +0900

    lesson 42 compute the normals part done

--- chapter_04_shaders/42-wobbly-sphere-shader/src/script.js
+++ chapter_04_shaders/42-wobbly-sphere-shader/src/script.js
@@ -7,6 +7,7 @@ import CustomShaderMaterial from 'three-custom-shader-material/vanilla'
 import GUI from 'lil-gui'
 import wobbleVertexShader from './shaders/wobble/vertex.glsl'
 import wobbleFragmentShader from './shaders/wobble/fragment.glsl'
+import { mergeVertices } from 'three/examples/jsm/utils/BufferGeometryUtils.js'
 
 /**
  * Base
@@ -70,7 +71,9 @@ gui.add(material, 'thickness', 0, 10, 0.001)
 gui.addColor(material, 'color')
 
 // Geometry
-const geometry = new THREE.IcosahedronGeometry(2.5, 50)
+let geometry = new THREE.IcosahedronGeometry(2.5, 50)
+geometry = mergeVertices(geometry)
+geometry.computeTangents()
 
 // Mesh
 const wobble = new THREE.Mesh(geometry, material)

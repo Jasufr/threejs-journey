a6ebde6db5ea78f4bd38d37ebe6eb13f523aefc6
justin etienne <juetienne7@gmail.com>
2024-09-19 10:51:42 +0900

    lesson 44 color part done

--- chapter_04_shaders/44-procedural-terrain-shader/src/shaders/terrain/vertex.glsl
+++ chapter_04_shaders/44-procedural-terrain-shader/src/shaders/terrain/vertex.glsl
@@ -1,12 +1,17 @@
+uniform float uTime;
 uniform float uPositionFrequency;
 uniform float uStrength;
 uniform float uWarpFrequency;
 uniform float uWarpStrength;
 
+varying vec3 vPosition;
+varying float vUpDot;
+
 #include ../includes/simplexNoise2d.glsl
 
 float getElevation(vec2 position) {
   vec2 warpedPosition = position;
+  warpedPosition += uTime * 0.2;
   warpedPosition += simplexNoise2d(warpedPosition * uPositionFrequency * uWarpFrequency) * uWarpStrength;
 
   float elevation = 0.0;
@@ -37,4 +42,9 @@ void main() {
   vec3 toA = normalize(positionA - csm_Position);
   vec3 toB = normalize(positionB - csm_Position);
   csm_Normal = cross(toA, toB);
+
+  // Varyings
+  vPosition = csm_Position;
+  vPosition.xz += uTime * 0.2;
+  vUpDot = dot(csm_Normal, vec3(0.0, 1.0, 0.0));
 }

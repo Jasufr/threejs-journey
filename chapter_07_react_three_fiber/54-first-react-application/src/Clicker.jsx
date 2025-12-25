80ab4af725f8293b7d793959ec0bad8d0f8400bc
Justin Etienne <juetienne7@gmail.com>
2025-12-26 03:59:05 +0900

    lesson 54 clicker component created

---
+++ chapter_07_react_three_fiber/54-first-react-application/src/Clicker.jsx
@@ -0,0 +1,8 @@
+export default function Clicker() {
+  return (
+    <div>
+      <div>Clicks count: 0</div>
+      <button>Click me</button>
+    </div>
+  );
+}
---
+++ chapter_07_react_three_fiber/54-first-react-application/src/index.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./style.css";
+
+const root = createRoot(document.querySelector("#root"));
+const toto = "there";
+
+root.render(
+  <>
+    <App />
+  </>
+);

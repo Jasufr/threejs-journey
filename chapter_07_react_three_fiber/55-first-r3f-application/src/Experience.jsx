ab169981529c63a8f63d0f6047842ce8b4343b4c
Justin Etienne <juetienne7@gmail.com>
2026-01-12 02:14:26 +0900

    lesson 55 creating and handling meshes part done

--- chapter_07_react_three_fiber/55-first-r3f-application/src/Experience.jsx
+++ chapter_07_react_three_fiber/55-first-r3f-application/src/Experience.jsx
@@ -1,9 +1,12 @@
 export default function Experience() {
   return (
     <>
-      <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+        {/* <torusKnotGeometry /> */}
+        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+        <boxGeometry />
+        {/* <meshNormalMaterial /> */}
+        <meshBasicMaterial color="mediumpurple" wireframe />
       </mesh>
     </>
   );

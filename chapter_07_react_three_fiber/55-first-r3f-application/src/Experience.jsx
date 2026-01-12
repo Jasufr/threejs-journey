38fb7228c71dec924d767c74889a89bcf93512d5
Justin Etienne <juetienne7@gmail.com>
2026-01-12 19:56:18 +0900

    lesson 55 orbit controls part done

--- chapter_07_react_three_fiber/55-first-r3f-application/src/Experience.jsx
+++ chapter_07_react_three_fiber/55-first-r3f-application/src/Experience.jsx
@@ -1,12 +1,38 @@
+import { extend, useFrame, useThree } from "@react-three/fiber";
+import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+extend({ OrbitControls });
+
 export default function Experience() {
+  const { camera, gl } = useThree();
+  const cubeRef = useRef();
+  const groupRef = useRef();
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+    // groupRef.current.rotation.y += delta;
+  });
   return (
     <>
-      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-        {/* <torusKnotGeometry /> */}
-        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
-        <boxGeometry />
-        {/* <meshNormalMaterial /> */}
-        <meshBasicMaterial color="mediumpurple" wireframe />
+      <orbitControls args={[camera, gl.domElement]} />
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+        </mesh>
+        <mesh
+          ref={cubeRef}
+          rotation-y={Math.PI * 0.25}
+          position-x={2}
+          scale={1.5}
+        >
+          <boxGeometry scale={1.5} />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+      </group>
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
       </mesh>
     </>
   );

7525fae4339d5e31c9a404e271729e8089714c7c
Justin Etienne <juetienne7@gmail.com>
2026-01-14 03:06:56 +0900

    lesson 55 custom geometry part done

--- chapter_07_react_three_fiber/55-first-r3f-application/src/CustomObject.jsx
+++ chapter_07_react_three_fiber/55-first-r3f-application/src/CustomObject.jsx
@@ -1,14 +1,36 @@
-export default function CustomObject() {
-  const verticesCount = 10 * 3;
-  const positions = new Float32Array(verticesCount * 3);
+import { useEffect, useMemo, useRef } from "react";
+import * as THREE from "three";
 
-  for(let i = 0; i < verticesCount * 3; i++) {
-    positions[i] =( Math.random() - 0.5) * 3
-  }
-  return <mesh>
-    <bufferGeometry>
-      <bufferAttribute attach="attributes-position" count={verticesCount} itemSize={3} array={positions}  />
-    </bufferGeometry>
-    <meshBasicMaterial color="red" />
-  </mesh>
+export default function CustomObject() {
+  const geometryRef = useRef();
+
+  const verticesCount = 10 * 3;
+
+  const positions = useMemo(() => {
+    const positions = new Float32Array(verticesCount * 3);
+
+    for (let i = 0; i < verticesCount * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 3;
+    }
+
+    return positions;
+  }, []);
+
+  useEffect(() => {
+    geometryRef.current.computeVertexNormals();
+  }, []);
+
+  return (
+    <mesh>
+      <bufferGeometry ref={geometryRef}>
+        <bufferAttribute
+          attach="attributes-position"
+          count={verticesCount}
+          itemSize={3}
+          array={positions}
+        />
+      </bufferGeometry>
+      <meshStandardMaterial color="red" side={THREE.DoubleSide} />
+    </mesh>
+  );
 }

fbc8f12a907cf462dd146a4a0e35047e293e02a9
Justin Etienne <juetienne7@gmail.com>
2026-01-14 04:31:21 +0900

    lesson 55 done

--- chapter_07_react_three_fiber/55-first-r3f-application/src/Experience.jsx
+++ chapter_07_react_three_fiber/55-first-r3f-application/src/Experience.jsx
@@ -10,13 +10,18 @@ export default function Experience() {
   const cubeRef = useRef();
   const groupRef = useRef();
   useFrame((state, delta) => {
+    // const angle = state.clock.elapsedTime;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0);
+
     cubeRef.current.rotation.y += delta;
     // groupRef.current.rotation.y += delta;
   });
   return (
     <>
       <orbitControls args={[camera, gl.domElement]} />
-      <directionalLight position={[1,2,3]} intensity={4.5} />
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
       <group ref={groupRef}>
         <mesh position-x={-2}>

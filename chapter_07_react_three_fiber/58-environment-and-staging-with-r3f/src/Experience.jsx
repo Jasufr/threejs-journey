af1f287d68daaec8195b423b128a7ed87f8a4051
Justin Etienne <juetienne7@gmail.com>
2026-02-07 14:23:16 +0900

    lesson 58 shadows part done

---
+++ chapter_07_react_three_fiber/58-environment-and-staging-with-r3f/src/Experience.jsx
@@ -0,0 +1,111 @@
+import { useFrame } from "@react-three/fiber";
+import {
+  OrbitControls,
+  useHelper,
+  BakeShadows,
+  SoftShadows,
+  AccumulativeShadows,
+  RandomizedLight,
+  ContactShadows,
+} from "@react-three/drei";
+import { useRef } from "react";
+import { Perf } from "r3f-perf";
+import * as THREE from "three";
+import { useControls } from "leva";
+
+export default function Experience() {
+  const directionalLight = useRef();
+  useHelper(directionalLight, THREE.DirectionalLightHelper, 1);
+
+  const cube = useRef();
+
+  useFrame((state, delta) => {
+    // const time = state.clock.elapsedTime;
+    // cube.current.position.x = 2 + Math.sin(time);
+    cube.current.rotation.y += delta * 0.2;
+  });
+
+  const { color, opacity, blur } = useControls("contact shadows", {
+    color: "#1d8f75",
+    opacity: { value: 0.4, min: 0, max: 1 },
+    blur: { value: 2.8, min: 0, max: 10 },
+  });
+
+  return (
+    <>
+      {/* <BakeShadows /> */}
+      {/* <SoftShadows size={25} samples={10} focus={0} /> */}
+
+      <color args={["ivory"]} attach="background" />
+
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      {/* <AccumulativeShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        color="#316D39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+        blend={100}
+      >
+        <RandomizedLight
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={3}
+          position={[1, 2, 3]}
+          bias={0.001}
+        />
+      </AccumulativeShadows> */}
+
+      <ContactShadows
+        position={[0, -0.99, 0]}
+        scale={10}
+        resolution={512}
+        far={5}
+        color={color}
+        opacity={opacity}
+        blur={blur}
+        frames={1}
+      />
+
+      <directionalLight
+        ref={directionalLight}
+        position={[1, 2, 3]}
+        intensity={1.5}
+        castShadow
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-bottom={-5}
+        shadow-camera-left={-5}
+      />
+      <ambientLight intensity={1.5} />
+
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh castShadow ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh
+        // receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+}

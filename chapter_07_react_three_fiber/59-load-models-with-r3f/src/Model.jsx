2da2c4d1c52e80b59796518ea434b0e88bb6c2b2
Justin Etienne <juetienne7@gmail.com>
2026-02-14 17:26:06 +0900

    lesson 59 gltf loading with drei part started

--- chapter_07_react_three_fiber/59-load-models-with-r3f/src/Model.jsx
+++ chapter_07_react_three_fiber/59-load-models-with-r3f/src/Model.jsx
@@ -1,12 +1,9 @@
+import { useGLTF } from "@react-three/drei";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 
 export default function Model() {
-  const model = useLoader(GLTFLoader, "./hamburger.glb", (loader) => {
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath("./draco/");
-    loader.setDRACOLoader(dracoLoader);
-  });
+  const model = useGLTF("./hamburger.glb");
   return <primitive object={model.scene} scale={0.35} position-y={-1} />;
 }

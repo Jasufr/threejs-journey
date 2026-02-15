6fe10385b2f9f52aa2181b30c4b5c89d492c15a5
Justin Etienne <juetienne7@gmail.com>
2026-02-15 13:10:20 +0900

    lesson 59 done

--- chapter_07_react_three_fiber/59-load-models-with-r3f/src/Model.jsx
+++ chapter_07_react_three_fiber/59-load-models-with-r3f/src/Model.jsx
@@ -1,8 +1,19 @@
-import { useGLTF } from "@react-three/drei";
+import { Clone, useGLTF } from "@react-three/drei";
 
 export default function Model() {
   const model = useGLTF("./hamburger-draco.glb");
-  return <primitive object={model.scene} scale={0.35} position-y={-1} />;
+  return (
+    <>
+      <Clone
+        object={model.scene}
+        scale={0.35}
+        position-y={-1}
+        position-x={-4}
+      />
+      <Clone object={model.scene} scale={0.35} position-y={-1} position-x={0} />
+      <Clone object={model.scene} scale={0.35} position-y={-1} position-x={4} />
+    </>
+  );
 }
 
 useGLTF.preload("./hamburger-draco.glb");

4d9d5abbe6ae9c8102f4a968c80fad27b1378a45
Justin Etienne <juetienne7@gmail.com>
2026-02-15 15:47:52 +0900

    lesson 60 done

--- chapter_07_react_three_fiber/60-3d-text-with-r3f/src/Experience.jsx
+++ chapter_07_react_three_fiber/60-3d-text-with-r3f/src/Experience.jsx
@@ -4,22 +4,53 @@ import {
   Center,
   useMatcapTexture,
 } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { Perf } from "r3f-perf";
+import { useState, useEffect, useRef } from "react";
+import * as THREE from "three";
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
 
 export default function Experience() {
-  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
-  // const tempArray = [...Array(100)]
-  // tempArray.map(() => {
+  // const [torusGeometry, setTorusGeometry] = useState();
+  // const [material, setMaterial] = useState();
 
-  // })
+  // const donutsGroup = useRef();
+
+  const donuts = useRef([]);
+
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
+
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
+
+  useFrame((state, delta) => {
+    // for (const donut of donutsGroup.current.children) {
+    //   donut.rotation.y += delta * 0.2;
+    // }
+    for (const donut of donuts.current) {
+      donut.rotation.y += delta * 0.2;
+    }
+  });
+
   return (
     <>
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
+      {/* <torusGeometry ref={setTorusGeometry} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} /> */}
+
       <Center>
         <Text3D
+          material={material}
           font="./fonts/helvetiker_regular.typeface.json"
           size={0.75}
           height={0.2}
@@ -31,13 +62,18 @@ export default function Experience() {
           bevelSegments={5}
         >
           HELLO R3F
-          <meshMatcapMaterial matcap={matcapTexture} />
         </Text3D>
       </Center>
 
+      {/* <group ref={donutsGroup}> */}
       {[...Array(100)].map((value, index) => (
         <mesh
+          ref={(element) => {
+            donuts.current[index] = element;
+          }}
           key={index}
+          geometry={torusGeometry}
+          material={material}
           position={[
             (Math.random() - 0.5) * 10,
             (Math.random() - 0.5) * 10,
@@ -45,11 +81,9 @@ export default function Experience() {
           ]}
           scale={0.2 + Math.random() * 0.2}
           rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
-        >
-          <torusGeometry />
-          <meshMatcapMaterial matcap={matcapTexture} />
-        </mesh>
+        />
       ))}
+      {/* </group> */}
     </>
   );
 }

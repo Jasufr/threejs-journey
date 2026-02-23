96d9dd7c7985b5e09600d215a7d59541df5d180f
Justin Etienne <juetienne7@gmail.com>
2026-02-23 20:34:39 +0900

    lesson 63 performance part done

--- chapter_07_react_three_fiber/63-post-processing-with-r3f/src/Experience.jsx
+++ chapter_07_react_three_fiber/63-post-processing-with-r3f/src/Experience.jsx
@@ -1,31 +1,69 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import {
+  ToneMapping,
+  EffectComposer,
+  Vignette,
+  Glitch,
+  Noise,
+  Bloom,
+  DepthOfField,
+} from "@react-three/postprocessing";
+import { ToneMappingMode, BlendFunction, GlitchMode } from "postprocessing";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  return (
+    <>
+      <color args={["#ffffff"]} attach="background" />
 
-        <Perf position="top-left" />
+      <EffectComposer>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
+        {/* <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.2, 0.4]}
+          mode={GlitchMode.CONSTANT_WILD}
+        /> */}
+        {/* <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} /> */}
+        {/* <Bloom luminanceThreshold={1.1} mipmapBlur intensity={0.5} /> */}
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        /> */}
+        <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
+      </EffectComposer>
 
-        <OrbitControls makeDefault />
+      <Perf position="top-left" />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+      <OrbitControls makeDefault />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
 
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }

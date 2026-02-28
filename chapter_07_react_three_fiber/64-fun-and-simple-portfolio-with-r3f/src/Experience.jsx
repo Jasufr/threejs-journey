1fcfb18454bc892884bc5130e449d8269e71d8d8
Justin Etienne <juetienne7@gmail.com>
2026-02-28 20:26:27 +0900

    lesson 64 laptop part done

--- chapter_07_react_three_fiber/64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
+++ chapter_07_react_three_fiber/64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -1,15 +1,20 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const computer = useGLTF("/models/macbook_model.gltf");
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
+  return (
+    <>
+      <color args={["#241a1a"]} attach="background" />
 
+      <OrbitControls makeDefault />
+
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+
+      <primitive object={computer.scene} />
     </>
+  );
 }

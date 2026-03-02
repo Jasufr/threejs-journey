c84cf03f95594e0395abf50c07389f35517ff376
Justin Etienne <juetienne7@gmail.com>
2026-03-02 22:14:06 +0900

    lesson 65 object settings part done until mass

--- chapter_07_react_three_fiber/65-physics-with-r3f/src/Experience.jsx
+++ chapter_07_react_three_fiber/65-physics-with-r3f/src/Experience.jsx
@@ -11,7 +11,8 @@ import { useRef } from "react";
 export default function Experience() {
   const cube = useRef();
   const cubeJump = () => {
-    cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    const mass = cube.current.mass();
+    cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 });
     cube.current.applyTorqueImpulse({
       x: Math.random() - 0.5,
       y: Math.random() - 0.5,
@@ -27,7 +28,7 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <Physics debug>
+      <Physics gravity={[0, -9.08, 0]} debug>
         <RigidBody colliders="ball">
           <mesh castShadow position={[-1.5, 2, 0]}>
             <sphereGeometry />
@@ -35,14 +36,22 @@ export default function Experience() {
           </mesh>
         </RigidBody>
 
-        <RigidBody position={[1.5, 2, 0]} ref={cube}>
+        <RigidBody
+          position={[1.5, 2, 0]}
+          ref={cube}
+          gravityScale={1}
+          restitution={0}
+          friction={0.7}
+          colliders={false}
+        >
           <mesh castShadow onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
+          <CuboidCollider mass={2} args={[0.5, 0.5, 0.5]} />
         </RigidBody>
 
-        <RigidBody type="fixed">
+        <RigidBody type="fixed" friction={0.7}>
           <mesh receiveShadow position-y={-1.25}>
             <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
